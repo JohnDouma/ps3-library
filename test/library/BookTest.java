2ed7fddadb423c02d2883d39f3eb5ec1e4daad16
John Douma <jdouma@amazon.com>
2016-11-21 21:27:14 -0500

    Added unit tests to BookTest.java

--- test/library/BookTest.java
+++ test/library/BookTest.java
@@ -1,9 +1,12 @@
 package library;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  * Test suite for Book ADT.
@@ -28,6 +31,48 @@ public class BookTest {
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
+    }
+    
+    /*
+     * Testing strategy
+     * ==================
+     * 
+     * Test Book creation with invalid title
+     * Test Book creation with empty author list
+     * Test Book creation with invalid author
+     * Test Book creation with zero year and negative year
+     * Try to modify authors list returned from Book#getAuthors
+     */
+    
+    @Test(expected=AssertionError.class)
+    public void testBookCreationWithInvalidTitle() {
+        new Book("", Arrays.asList("Fred Bloggs"), 2016);
+    }
+    
+    @Test(expected=AssertionError.class)
+    public void testBookCreationWithZeroYear() {
+        new Book("The Book", Arrays.asList("Fred Bloggs"), 0);
+    }
+    
+    @Test(expected=AssertionError.class)
+    public void testBookCreationWithNegativeYear() {
+        new Book("The Book", Arrays.asList("Fred Bloggs"), -1);
+    }
+    
+    @Test(expected=AssertionError.class)
+    public void testBookCreationWithEmptyAuthorList() {
+        new Book("The Book", new ArrayList<String>(), 1990);
+    }
+    
+    @Test(expected=AssertionError.class)
+    public void testBookCreationWithInvalidAuthor() {
+        new Book("The Book", Arrays.asList("Fred Bloggs", " "), 1990);
+    }
+    
+    @Test(expected=UnsupportedOperationException.class)
+    public void testTryToModifyAuthorList() {
+        Book book = new Book("The Book", Arrays.asList("Fred Bloggs"), 1990);
+        book.getAuthors().add("This should throw an exception");
     }
 
     /* Copyright (c) 2016 MIT 6.005 course staff, all rights reserved.
